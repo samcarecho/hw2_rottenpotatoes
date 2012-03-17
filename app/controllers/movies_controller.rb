225827c3ede0d8cc0e7438318ba21eb6a0f3875d
Sam Carecho <digiwise@gmail.com>
2012-03-16 23:30:56 -0700

    initial commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,55 @@
+class MoviesController < ApplicationController
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    sort = params[:sort]
+    case sort
+      when "title"
+        @movies = Movie.all(:order => "title")
+        @hiliteA = "hilite"
+        @hiliteB = ""
+      when "release_date"
+        @movies = Movie.all(:order => "release_date")
+        @hiliteA = ""
+        @hiliteB = "hilite"
+      else
+        @movies = Movie.all
+        @hiliteA = ""
+        @hiliteB = ""
+    end  
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
